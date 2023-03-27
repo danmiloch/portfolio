5923951213e44650284faff39bdb190a159aa4a2
Dan Miloch <hellodan@hey.com>
2023-03-26 20:49:36 -0400

    try again

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,18 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   integrations: [mdx()]
+  //   markdown: {
+  //   syntaxHighlight: 'shiki',
+  //   shikiConfig: {
+  //     // Choose from Shiki's built-in themes (or add your own)
+  //     // https://github.com/shikijs/shiki/blob/main/docs/themes.md
+  //     theme: 'material-theme-palenight',
+  //     // Add custom languages
+  //     // Note: Shiki has countless langs built-in, including .astro!
+  //     // https://github.com/shikijs/shiki/blob/main/docs/languages.md
+  //     langs: [],
+  //     // Enable word wrap to prevent horizontal scrolling
+  //     wrap: true,
+  //   },
+  // },
 });
